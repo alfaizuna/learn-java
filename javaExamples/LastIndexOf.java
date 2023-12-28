5f2fd3ba5a994fce5769ad07b435d7e32f049d55
alfaizuna <alfaizunaulia@gmail.com>
2023-12-28 15:19:17 +0700

    lst of index

---
+++ javaExamples/LastIndexOf.java
@@ -0,0 +1,9 @@
+package javaExamples;
+
+public class LastIndexOf {
+    public static void main(String[] args) {
+        String t1 = "Alfaizuna Aulia";
+        int index = t1.lastIndexOf("Aulia");
+        System.out.println(index);
+    }
+}
