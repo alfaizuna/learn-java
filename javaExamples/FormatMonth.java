f8130d5efad6ecb8f5080b1f2aa38f91bdf93bf2
alfaizuna <alfaizunaulia@gmail.com>
2024-01-02 17:55:51 +0700

    add learning

--- javaExamples/FormatMonth.java
+++ javaExamples/FormatMonth.java
@@ -7,7 +7,6 @@ public class FormatMonth {
     public static void main(String[] args) {
         Formatter fmt = new Formatter();
         Calendar cal = Calendar.getInstance();
-        fmt = new Formatter();
         fmt.format("%tB %tb %tm", cal, cal, cal);
         System.out.println(fmt);
     }
