11fc3dc581de7a979dacdbefb050bc61140ae97d
alfaizuna <alfaizunaulia@gmail.com>
2024-01-02 17:53:53 +0700

    SimpleDateFormatExample

---
+++ javaExamples/SimpleDateFormatExample.java
@@ -0,0 +1,13 @@
+package javaExamples;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class SimpleDateFormatExample {
+    public static void main(String[] args) {
+        Date date = new Date();
+        String strDateFormat = "HH:mm:ss a";
+        SimpleDateFormat sdf = new SimpleDateFormat(strDateFormat);
+        System.out.println(sdf.format(date));
+    }
+}
